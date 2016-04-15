f06fe314b9fec1b6bfab03c763c2736c940fdeb1
Alan Tai <alan@9gag.com>
2016-04-15 21:10:52 +0800

    Housekeep AJAX connections

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('sdk/page-mod').PageMod({
   include: 'https://github.com*',
   contentScriptFile: [
     './jquery-2.2.2.min.js',
+    './helper.js',
     './approvers.js',
     './profile.js',
     './github-extensions.js'
